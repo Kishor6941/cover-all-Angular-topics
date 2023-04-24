da284ceb7eaad7c503a2c0221e880f454b90a056
Kishor6941 <phawadeks9049@gmail.com>
2023-04-24 13:32:53 +0530

    ngOnchanges

--- angular-pratice/src/app/app-routing.module.ts
+++ angular-pratice/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LifeCycleComponent } from './life-cycle/life-cycle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : NavbarComponent,
+    children :[
+      {
+        path : 'lifeCycle',
+        component : LifeCycleComponent
+       }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-pratice/src/app/app.module.ts
+++ angular-pratice/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LifeCycleComponent } from './life-cycle/life-cycle.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ChildCompComponent } from './life-cycle/child-comp/child-comp.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LifeCycleComponent,
+    NavbarComponent,
+    ChildCompComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
