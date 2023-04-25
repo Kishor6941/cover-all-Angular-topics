a2a0be0fa8439eb3bf61e1263590e8d251f283ef
Kishor6941 <phawadeks9049@gmail.com>
2023-04-25 20:21:57 +0530

    routing

--- angular-pratice/src/app/app-routing.module.ts
+++ angular-pratice/src/app/app-routing.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LifeCycleComponent } from './life-cycle/life-cycle.component';
+import { UserComponent } from './user/user.component';
+import { AboutComponent } from './about/about.component';
+import { TdfComponent } from './forms/tdf/tdf.component';
+import { ReactiveComponent } from './forms/reactive/reactive.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ViewUserComponent } from './user/view-user/view-user.component';
+import { LocationComponent } from './location/location.component';
+import { FeedComponent } from './feed/feed.component';
 
 const routes: Routes = [
   {
@@ -11,9 +19,45 @@ const routes: Routes = [
       {
         path : 'lifeCycle',
         component : LifeCycleComponent
+       },
+       {
+        path : 'users',
+        component : UserComponent,
+       },
+       {
+        path : 'users/:id',
+        component : ViewUserComponent,
+       },
+       {
+        path : 'about',
+        component : AboutComponent,
+        children : [
+          {
+            path : 'location',
+            outlet : 'location',
+            component : LocationComponent
+          },
+          {
+            path : 'feed',
+            outlet : 'feed',
+            component:FeedComponent
+          }
+        ]
+       },
+       {
+        path : 'tdf',
+        component : TdfComponent
+       },
+       {
+        path : 'reactive',
+        component : ReactiveComponent
        }
     ]
   },
+  {
+    path:"**",
+    component:PageNotFoundComponent
+  }
 ];
 
 @NgModule({
